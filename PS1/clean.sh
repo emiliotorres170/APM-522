5e153de8e5929c37d583e71f45a43d43a8dd7253
Emilio Torres <Emilio Torres>
2018-08-29 20:08:39 -0700

    initial commit of the reps

---
+++ PS1/clean.sh
@@ -0,0 +1,15 @@
+clear
+rm -f *.aux
+rm -f *.bbl
+rm -f *.log
+rm -f *.pdf
+rm -f *.toc
+rm -f *.out
+rm -f *.blg
+rm -f *.lot
+rm -f *.xwm
+rm -f *.lof
+rm -f *.ind
+rm -f *.idx
+rm -f *.ilg
+rm -f *.nlo
